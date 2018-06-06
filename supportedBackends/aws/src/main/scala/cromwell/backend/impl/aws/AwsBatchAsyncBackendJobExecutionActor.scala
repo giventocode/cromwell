aa99ec203d4cf17d4ad0d52075f9b185f01b8ea2
Emil Lerch <emil@lerch.org>
2018-06-06 03:42:35 +0000

    write output to S3 on completion

--- supportedBackends/aws/src/main/scala/cromwell/backend/impl/aws/AwsBatchAsyncBackendJobExecutionActor.scala
+++ supportedBackends/aws/src/main/scala/cromwell/backend/impl/aws/AwsBatchAsyncBackendJobExecutionActor.scala
@@ -369,22 +369,15 @@ class AwsBatchAsyncBackendJobExecutionActor(override val standardParams: Standar
    *
    */
   private def writeToPaths(jobId: String, job: AwsBatchJob) = {
-    import cromwell.core.path.DefaultPathBuilder
+    // import cromwell.filesystems.s3.S3PathBuilder
     val detail = job.detail(jobId)
+    // TODO: Apparently in a bad config job.rc(detail) can be null, which
+    //       ends up not writing the returnCode file. This fails the job, for
+    //       a failed job, so not sure it's a real problem at this time
     Log.info("Job Complete. Exit code: " + job.rc(detail))
-    // TODO: Woah! Is this ugly! We really need to unwind these three lines
-    //       and figure out why we're getting a combined callRoot + standardPaths.output
-    //       The problem here is that output validation comes from a Future
-    //       somewhere and it's been elusive to track down.
-    val combinedstdoutPath = DefaultPathBuilder.get(jobPaths.callRoot + jobPaths.standardPaths.output.toString)
-    combinedstdoutPath.createIfNotExists(asDirectory = false, createParents = true)
-    combinedstdoutPath.write(job.output(detail))
+    Log.info("Output path: " + jobPaths.standardPaths.output.toString)
 
-    // The rest of this is relatively cool, although it doesn't belong in this part of the
-    // lifecycle
-    jobPaths.standardPaths.output.createIfNotExists(asDirectory = false, createParents = true)
-    jobPaths.standardPaths.error.createIfNotExists(asDirectory = false, createParents = true)
-    jobPaths.returnCode.createIfNotExists(asDirectory = false, createParents = true)
+    // TODO: Is this the right place to be writing out to S3?
     jobPaths.standardPaths.output.write(job.output(detail))
     jobPaths.standardPaths.error.touch()
     jobPaths.returnCode.write(job.rc(detail).toString)
